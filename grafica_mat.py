2d9753675bc254bc65228bbb1345e60babbb91ac
alu0100956955 <43880851+alu0100956955@users.noreply.github.com>
2020-03-30 23:26:03 +0100

    Parametrizar la grafica de tipo lineas matplotlib
    He añadido 3 metodos para que aunque se aumente la cantidad de paises todos tendran un tipo de linea, color y markador distinto (hasta cierto punto)

--- grafica_mat.py
+++ grafica_mat.py
@@ -17,19 +17,34 @@ class Linea_mat(Grafica):
                 i +=1
 
 # TO DO:
-# parametrizar el pais que pueda mostrar
+# El metodo para poder rotar entre los distintso tipos de lineas ( mirar hojas)
 # probar mas tipos de graficas
 # Permitir distintos tipos de graficas o tener distintos metodos para hacere las graficas
     def show(l):
-        #plt.plot(l.ejeX(),l.ejeY(18), 'b--', marker='.', label = 'España')
-        #plt.plot(l.ejeX(),l.ejeY(16), 'g--', marker='^', label = 'Italia')
-        plt.plot(l.ejeX(),l.ejeY("Spain"), 'b--', marker='.', label = 'España')
-        plt.plot(l.ejeX(),l.ejeY("Italy"), 'g--', marker='^', label = 'Italia')
+        paises = ['Spain','Italy','China','Portugal']
+        for i in range(len(paises)):
+            plt.plot(l.ejeX(),l.ejeY(paises[i]), marker = Linea_mat.marker(i), linestyle = Linea_mat.line(i), markeredgecolor = Linea_mat.color(i), label = paises[i]) # TO DO ver como alterar el tipo de grafica y sus opciones en cada iteracion
         plt.xticks(ticks=l.rango(),labels=l.ejeX(),rotation=70)
         plt.title('Casos confirmados')
         plt.legend()
         plt.grid()
         plt.show()
+
+# Controlar cuando se salga del limite
+    # Metodo que devuelve el tipo de marca
+    def marker(indice):
+        markers = ['.', ',', 'o', 'v', '^', '<', '>', '1', '2', '3', '4', 's', 'p', '*', 'h', 'H', '+', 'x', 'D', 'd', '|', '_' ]
+        return markers[indice]
+
+    # Metodo que devuelve el tipo de linea
+    def line(indice):
+        line = ['-', '--', '-.', ':' ]
+        return line[indice]
+
+    # Metodo que devuelve el color de la linea
+    def color(indice):
+        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w' ]
+        return colors[indice]
 
     def prueba():
         a = l.ejeX(4)
